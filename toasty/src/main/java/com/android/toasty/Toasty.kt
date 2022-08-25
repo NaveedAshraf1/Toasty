d63d59dfa230010bfc6088e3d224c090c3dc4354
Naveed <naveedashraf3455@gmail.com>
2022-08-25 07:59:22 +0500

    Initial commit

---
+++ toasty/src/main/java/com/android/toasty/Toasty.kt
@@ -0,0 +1,19 @@
+package com.android.toasty
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * @Author: Harry
+ * @Date: 8/25/2022
+ */
+class Toasty {
+
+    companion object {
+
+        fun simpleToast(context: Context, msg: String) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
